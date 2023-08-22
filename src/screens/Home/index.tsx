1268f6a0a0d23f2ff0e91ed61092fd7b874583d6
Rafael Gomes <rafaell.gomes@gmail.com>
2023-08-21 23:37:43 -0300

    Initiate the home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,39 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+
+import { styles } from "./styles";
+
+export function Home() {
+  
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de adicionar")
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text  style={styles.eventDate}>
+        Segunda, 21 de agosto de 2023
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity 
+          style={styles.button} 
+          onPress={handleParticipantAdd}
+        >
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
